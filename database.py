870dcd7ccb780f4e661617b4c5320eedf4f707ce
east-ant <eve188811@gmail.com>
2026-05-23 19:04:45 +0900

    db rollback

--- database.py
+++ database.py
@@ -11,8 +11,7 @@ def get_connection():
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME"),
-        charset="utf8mb4",
-        ssl={"ssl_disabled": False}
+        charset="utf8mb4"
     )
 
 def init_db():
